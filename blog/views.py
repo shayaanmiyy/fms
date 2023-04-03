fd23c4742637e7a29ed3e9b60a2ba9d4f6fab6f0
Shayaan Momin <>
2023-04-03 11:47:23 +0000

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,19 @@
+# importing models and libraries
+from django.shortcuts import render
+from .models import posts
+from django.views import generic
+from django.views.decorators.http import require_GET
+from django.http import HttpResponse
+
+
+# class based views for posts
+class postslist(generic.ListView):
+    queryset = posts.objects.filter(status=1).order_by('-created_on')
+    template_name = 'home.html'
+    paginate_by = 4
+
+
+# class based view for each post
+class postdetail(generic.DetailView):
+    model = posts
+    template_name = "post.html"
---
+++ venv/lib/python3.8/site-packages/feeds/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FeedsConfig(AppConfig):
+    name = 'feeds'
+    default_auto_field = 'django.db.models.AutoField'
